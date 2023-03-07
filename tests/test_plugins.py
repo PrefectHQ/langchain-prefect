f928c6ffb97a3b2bec5f47c3c0b92d4affadea91
Nathan Nowack <thrast36@gmail.com>
2023-03-07 02:09:39 -0600

    fix test

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,6 +1,6 @@
 from langchain.llms import OpenAI
 
-from prefect_langchain.plugins import llm_invocation_summary
+from prefect_langchain.utilities import NotAnArtifact, llm_invocation_summary
 
 
 class TestParseInvocationSummary:
@@ -8,7 +8,11 @@ class TestParseInvocationSummary:
         """Test that LLM invocation summary is parsed correctly using a callable LLM."""
         llm_input = "What would be a good name for a company that makes colorful socks?"
 
-        artifact = llm_invocation_summary(OpenAI(), llm_input)
+        artifact = llm_invocation_summary(
+            OpenAI(), llm_input, invocation_fn=lambda x: None
+        )
+
+        assert isinstance(artifact, NotAnArtifact)
 
         assert artifact.content["llm_endpoint"] == "langchain.llms.openai"
-        assert artifact.content["text_input"] == llm_input
+        assert artifact.content["prompts"] == llm_input
